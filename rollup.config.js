06023f6f9b39f4a07fd8bf1e1782f388be62a423
Anthony Frehner <frehner@users.noreply.github.com>
2019-09-26 10:45:58 -0600

    minifying the built file (#29)
    * minifying the built file
    
    * 2.1.1
    
    * update main in package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
+import {terser} from 'rollup-plugin-terser'
 
 export default {
   input: 'src/single-spa-canopy.js',
   output: {
-    file: 'lib/single-spa-canopy.js',
+    file: 'lib/single-spa-canopy.min.js',
     name: 'single-spa-canopy',
     format: 'umd',
     sourcemap: true,
@@ -13,7 +14,7 @@ export default {
     resolve(),
     babel({
       exclude: 'node_modules/**'
-    })
-
+    }),
+    terser(),
   ]
 }
