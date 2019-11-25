71e951d916d6f005f8faf833d932461e3de4fbe3
Anthony Frehner <afrehner.work@gmail.com>
2019-11-25 14:54:02 -0700

    adding an additional system output bundle
    as part of that, added folders for /lib/umd/ and /lib/system/
    
    small update to dev depedendencies.

--- rollup.config.js
+++ rollup.config.js
@@ -2,19 +2,36 @@ import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import {terser} from 'rollup-plugin-terser'
 
-export default {
-  input: 'src/single-spa-canopy.js',
-  output: {
-    file: 'lib/single-spa-canopy.min.js',
-    name: 'single-spa-canopy',
-    format: 'umd',
-    sourcemap: true,
+export default [
+  {
+    input: 'src/single-spa-canopy.js',
+    output: {
+      file: 'lib/umd/single-spa-canopy.min.js',
+      name: 'single-spa-canopy',
+      format: 'umd',
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      terser(),
+    ]
   },
-  plugins: [
-    resolve(),
-    babel({
-      exclude: 'node_modules/**'
-    }),
-    terser(),
-  ]
-}
+  {
+    input: 'src/single-spa-canopy.js',
+    output: {
+      file: 'lib/system/single-spa-canopy.min.js',
+      format: 'system',
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      terser(),
+    ]
+  }
+]
