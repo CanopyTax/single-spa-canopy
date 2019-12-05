9aac3466fc2f7d4286a1b819aec17fa0768ee911
Anthony Frehner <frehner@users.noreply.github.com>
2019-12-05 11:18:36 -0700

    the real change here is the new exported const __esModule (#33)
    why? well, a long story short is that other apps NEED that value to know that single-spa-canopy has a default export. wihtout it, they wrap single-spa-canopy in another object that looks like this: {default: [module]}. so you have to do a .default.default. ugh.
    
    normally these things are handled by babel and/or bundlers (webpack, rollup). however, in our case, for some reason rollup has decided NOT to add that when you only have a default export. so we have to add it manually.

--- rollup.config.js
+++ rollup.config.js
@@ -2,35 +2,25 @@ import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import {terser} from 'rollup-plugin-terser'
 
-export default [
-  {
-    input: 'src/single-spa-canopy.js',
-    output: {
+export default {
+  input: 'src/single-spa-canopy.js',
+  output: [
+    {
       file: 'lib/amd/single-spa-canopy.min.js',
       format: 'amd',
       sourcemap: true,
     },
-    plugins: [
-      resolve(),
-      babel({
-        exclude: 'node_modules/**'
-      }),
-      terser(),
-    ]
-  },
-  {
-    input: 'src/single-spa-canopy.js',
-    output: {
+    {
       file: 'lib/system/single-spa-canopy.min.js',
       format: 'system',
       sourcemap: true,
-    },
-    plugins: [
-      resolve(),
-      babel({
-        exclude: 'node_modules/**'
-      }),
-      terser(),
-    ]
-  }
-]
+    }
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    terser(),
+  ]
+}
