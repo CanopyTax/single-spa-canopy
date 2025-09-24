420812b1f5f5c18c23486a92e956998fa6f6919c
Tony Lefler <tonylefler@gmail.com>
2025-09-24 00:34:23 -0600

    remove systemjs, module package

--- src/single-spa-canopy.js
+++ src/single-spa-canopy.js
@@ -1,9 +1,9 @@
-import deepMerge from 'deepmerge';
-import { setOrRemoveAllOverlays, getAppName } from './overlays.helpers.js'
+import deepMerge from "deepmerge";
+import { setOrRemoveAllOverlays, getAppName } from "./overlays.helpers.js";
 
 const defaultOpts = {
   domElementGetter: null,
-  position: 'relative',
+  position: "relative",
   setPublicPath: null, // a function that should do `path => __webpack_public_path__ = path`. Necessary for hot reloading
   hotload: {
     module: null, // Webpack's "module" object for the root javascript module of the child application. (module.exports, module.hot, etc)
@@ -24,7 +24,7 @@ const defaultOpts = {
 
 export const __esModule = true;
 export default function singleSpaCanopy(userOpts) {
-  if (typeof userOpts !== 'object') {
+  if (typeof userOpts !== "object") {
     throw new Error(`single-spa-canopy requires an opts object`);
   }
 
@@ -39,83 +39,83 @@ export default function singleSpaCanopy(userOpts) {
 }
 
 function getUrl(props) {
-  return SystemJS.locate
-    ? SystemJS.locate({
-        name: `${getAppName(props)}!sofe`,
-        metadata: {},
-        address: "",
-      })
-    : SystemJS.import("sofe").then(({ getServiceUrl, InvalidServiceName }) => {
-          try {
-            return getServiceUrl(getAppName(props));
-          } catch (e) {
-            if (e instanceof InvalidServiceName) {
-              console.warn(
-                `The single-spa child app name is not the same as the sofe service!
-                This means that hotloading will not work!`
-              );
-              return 'INVALID';
-            } else {
-              throw e;
-            }
-          }
-      });
+  // Helper to resolve module URL from import map
+  function resolveModuleUrl(moduleName) {
+    const importMap = document.querySelector('script[type="importmap"]');
+    if (importMap) {
+      const map = JSON.parse(importMap.textContent);
+      return map.imports[moduleName] || moduleName;
+    }
+    return moduleName;
+  }
+
+  const moduleName = `@canopytax/${getAppName(props)}`;
+  return Promise.resolve(resolveModuleUrl(moduleName));
 }
 
 function isOverridden(props) {
-  return SystemJS
-    .import('sofe')
-    .then(sofe => sofe.isOverride(getAppName(props)))
+  // Since we're moving away from SOFE, this can return false
+  // or implement your own override logic
+  return Promise.resolve(false);
 }
 
 function bootstrap(opts, props) {
-  return Promise
-    .resolve()
-    .then(() => {
-      const blockingPromises = [];
-      const moduleName = `${getAppName(props)}!sofe`;
+  return Promise.resolve().then(() => {
+    const blockingPromises = [];
+    const moduleName = `@canopytax/${getAppName(props)}`;
 
-      blockingPromises.push(Promise.all([getUrl(props), isOverridden(props)]).then(values => {
+    blockingPromises.push(
+      Promise.all([getUrl(props), isOverridden(props)]).then((values) => {
         const [url, isOverridden] = values;
-        const invalidName = url === 'INVALID';
+        const invalidName = url === "INVALID";
 
-        const webpackPublicPath = url.slice(0, url.lastIndexOf('/') + 1);
+        const webpackPublicPath = url.slice(0, url.lastIndexOf("/") + 1);
         let publicPathSet = false;
 
         if (opts.setPublicPath) {
-          opts.setPublicPath(webpackPublicPath)
-          publicPathSet = true
+          opts.setPublicPath(webpackPublicPath);
+          publicPathSet = true;
         }
 
-        const shouldHotload = !invalidName && isOverridden && opts.hotload.dev.enabled;
+        const shouldHotload =
+          !invalidName && isOverridden && opts.hotload.dev.enabled;
 
         if (shouldHotload) {
           if (!opts.hotload.module) {
-            console.warn(`single-spa-canopy: for application '${getAppName(props)}', hot reloading is enabled but the opts.module is undefined. Either turn off hot reloading in singleSpaCanopy config, or pass in the module object to single-spa-canopy`);
+            console.warn(
+              `single-spa-canopy: for application '${getAppName(props)}', hot reloading is enabled but the opts.module is undefined. Either turn off hot reloading in singleSpaCanopy config, or pass in the module object to single-spa-canopy`,
+            );
           }
 
           if (opts.hotload.module && !opts.hotload.module.hot) {
-            console.warn(`single-spa-canopy: for application '${getAppName(props)}', hot reloading is enabled but webpack hot reloading is not (module.hot is undefined). Either turn off hot reloading in the singleSpaCanopy config, or enable webpack hot reloading`);
+            console.warn(
+              `single-spa-canopy: for application '${getAppName(props)}', hot reloading is enabled but webpack hot reloading is not (module.hot is undefined). Either turn off hot reloading in the singleSpaCanopy config, or enable webpack hot reloading`,
+            );
           }
 
           if (opts.hotload.__webpack_require__) {
             opts.hotload.__webpack_require__.p = webpackPublicPath;
-            publicPathSet = true
+            publicPathSet = true;
           }
 
           if (!publicPathSet) {
-            console.warn('single-spa-canopy: for application \'' + getAppName(props) + '\', hot reloading is enabled but the application is not bundled with webpack, which is currently the only supported bundler for hot reloading. Please provide __webpack_require__ opt to singleSpaCanopprovide __webpack_require__ opt to singleSpaCanopy.');
+            console.warn(
+              "single-spa-canopy: for application '" +
+                getAppName(props) +
+                "', hot reloading is enabled but the application is not bundled with webpack, which is currently the only supported bundler for hot reloading. Please provide __webpack_require__ opt to singleSpaCanopprovide __webpack_require__ opt to singleSpaCanopy.",
+            );
           }
 
           if (opts.hotload.module && opts.hotload.module.hot) {
             opts.hotload.module.hot.accept();
             opts.hotload.module.hot.dispose(() => {
-              SystemJS
-                .import('single-spa')
-                .then(singleSpa => {
-                  singleSpa.unloadApplication(getAppName(props), {waitForUnmount: opts.hotload.dev.waitForUnmount});
+              import("single-spa")
+                .then((singleSpa) => {
+                  singleSpa.unloadApplication(getAppName(props), {
+                    waitForUnmount: opts.hotload.dev.waitForUnmount,
+                  });
                 })
-                .catch(err => {
+                .catch((err) => {
                   setTimeout(() => {
                     throw err;
                   });
@@ -129,51 +129,50 @@ function bootstrap(opts, props) {
             [getAppName(props)]: url,
           });
         }
-      }))
+      }),
+    );
 
-      return Promise.all(blockingPromises);
-    });
+    return Promise.all(blockingPromises);
+  });
 }
 
 function mount(opts, props) {
-  return Promise
-    .resolve()
-    .then(() => {
-      let overlayArray = []
-      if (opts.domElementGetter) {
-        const el = getDomEl(opts);
-        el.style.position = opts.position
-        window.addEventListener('cp:show-overlay-keypress', toggleOverlays);
-        window.addEventListener('single-spa:routing-event', toggleOverlays);
+  return Promise.resolve().then(() => {
+    let overlayArray = [];
+    if (opts.domElementGetter) {
+      const el = getDomEl(opts);
+      el.style.position = opts.position;
+      window.addEventListener("cp:show-overlay-keypress", toggleOverlays);
+      window.addEventListener("single-spa:routing-event", toggleOverlays);
 
-        opts.overlay._toggleOverlays = toggleOverlays;
+      opts.overlay._toggleOverlays = toggleOverlays;
 
-        function toggleOverlays() {
-          setOrRemoveAllOverlays(el, opts, props);
-        }
+      function toggleOverlays() {
+        setOrRemoveAllOverlays(el, opts, props);
       }
-    });
+    }
+  });
 }
 
 function unmount(opts) {
-  return Promise
-    .resolve()
-    .then(() => {
-      window.removeEventListener('cp:show-overlay-keypress', opts.overlay._toggleOverlays)
-      window.removeEventListener('single-spa:routing-event', opts.overlay._toggleOverlays)
-    })
+  return Promise.resolve().then(() => {
+    window.removeEventListener(
+      "cp:show-overlay-keypress",
+      opts.overlay._toggleOverlays,
+    );
+    window.removeEventListener(
+      "single-spa:routing-event",
+      opts.overlay._toggleOverlays,
+    );
+  });
 }
 
 function unload(opts, props) {
-  return Promise
-    .resolve()
-    .then(() => {
-      const serviceName = getAppName(props) + '!sofe';
-      const wasDeleted = SystemJS.delete(SystemJS.normalizeSync(serviceName));
-      if (!wasDeleted) {
-        throw new Error(`Could not unload application '${serviceName}'`);
-      }
-    })
+  return Promise.resolve().then(() => {
+    // ESM modules are cached by the browser, no manual cleanup needed
+    // This function can be simplified or removed entirely
+    console.log(`Unloading application: ${getAppName(props)}`);
+  });
 }
 
 function attemptDeleteDomNode(selector) {
@@ -189,39 +188,43 @@ function attemptDeleteDomNode(selector) {
 function getDomEl(opts) {
   const el = opts.domElementGetter();
   if (!el) {
-    throw new Error(`single-spa-canopy: domElementGetter did not return a valid DOM element`);
+    throw new Error(
+      `single-spa-canopy: domElementGetter did not return a valid DOM element`,
+    );
   }
 
   return el;
 }
 
 function forceSetPublicPath(config) {
-  validateConfig(config)
-  return Promise
-    .resolve()
-    .then(() => {
-      const blockingPromises = [];
-      const moduleName = `${getAppName(config)}!sofe`;
+  validateConfig(config);
+  return Promise.resolve().then(() => {
+    const blockingPromises = [];
+    const moduleName = `@canopytax/${getAppName(config)}`;
 
-      blockingPromises.push(Promise.all([getUrl(config), isOverridden(config)]).then(values => {
+    blockingPromises.push(
+      Promise.all([getUrl(config), isOverridden(config)]).then((values) => {
         const [url, isOverridden] = values;
 
-        const webpackPublicPath = url.slice(0, url.lastIndexOf('/') + 1);
+        const webpackPublicPath = url.slice(0, url.lastIndexOf("/") + 1);
 
         if (config.setPublicPath) {
-          config.setPublicPath(webpackPublicPath)
+          config.setPublicPath(webpackPublicPath);
         }
-      }))
+      }),
+    );
 
-      return Promise.all(blockingPromises).then(results => null);
-    })
+    return Promise.all(blockingPromises).then((results) => null);
+  });
 }
 
 function validateConfig(config) {
-  const name = getAppName(config)
+  const name = getAppName(config);
   if (name === undefined) {
-    throw new Error('cannot get appName - invalid config')
+    throw new Error("cannot get appName - invalid config");
   } else if (!config.setPublicPath) {
-    throw new Error('cannot set publicPath without a `setPublicPath` method on the configuration')
+    throw new Error(
+      "cannot set publicPath without a `setPublicPath` method on the configuration",
+    );
   }
 }
